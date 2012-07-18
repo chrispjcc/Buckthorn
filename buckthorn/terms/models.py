47473be6b965d4055b5d26b728ac7d8cce289116
David Hewitt <david.hewitt.92@gmail.com>
2012-07-18 20:57:37 +0100

    Added terms to settings.py and began to implement the terms model.

--- buckthorn/terms/models.py
+++ buckthorn/terms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Term(models.Model):
+    startDate = models.DateField()
+    endDate = models.DateField()
+    CODE_CHOICES = (
+        ('MT','Michaelmas'),
+        ('HT','Hilary'),
+        ('TT','Trinity'),
+    )
+    code = models.CharField(max_length=2,choices=CODE_CHOICES)
+    year = models.IntegerField()
+
+    def __unicode__(self):
+        return u'%s%s' % (self.code, self.year)
+
+    @staticmethod
+    def getCurrentTerm():
+        return None
